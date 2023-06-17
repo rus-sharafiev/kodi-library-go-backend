8a1f9e5047a24cf92da3b8547e5e1231c63ab092
Rus <rus.sharafiev@gmail.com>
2023-06-17 18:47:02 +0300

    Add endpoints to api

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"rus-sharafiev/kodi/movies"
+	"rus-sharafiev/kodi/tvs"
 	"rus-sharafiev/kodi/web"
 
 	"github.com/gorilla/mux"
@@ -12,11 +13,16 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	// Web server
-	router.Handle("/", web.Server())
 
 	// API
 	router.Handle("/api/movies", movies.Get()).Methods("GET")
+	router.Handle("/api/movies/{id}", movies.Get()).Methods("GET")
+
+	router.Handle("/api/tvs", tvs.Get()).Methods("GET")
+	router.Handle("/api/tvs/{id}", tvs.Get()).Methods("GET")
+
+	// Web server
+	router.PathPrefix("/").Handler(web.Server{StaticPath: "build", IndexPath: "index.html"})
 
 	fmt.Printf("\n\x1b[2mHTTP server is running on http://localhost:8088/\n\n\x1b[0m")
 	log.Fatal(http.ListenAndServe(":8088", router))
