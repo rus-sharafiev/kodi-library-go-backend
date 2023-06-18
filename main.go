7b126fcb1f06a7bdc3d7ee07609b76c4dd1237cc
Rus <rus.sharafiev@gmail.com>
2023-06-18 21:10:49 +0300

    Add cors handler

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"rus-sharafiev/kodi/web"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -29,7 +30,13 @@ func main() {
 	// Web server
 	router.PathPrefix("/").Handler(web.Server{StaticPath: "build", IndexPath: "index.html"})
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8000"},
+		AllowCredentials: true,
+	})
+	handler := c.Handler(router)
+
 	// Start server
 	fmt.Printf("\n\x1b[2mHTTP server is running on http://localhost:8088/\n\n\x1b[0m")
-	log.Fatal(http.ListenAndServe(":8088", router))
+	log.Fatal(http.ListenAndServe(":8088", handler))
 }
