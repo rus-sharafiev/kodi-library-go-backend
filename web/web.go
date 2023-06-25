42115ded6547ceff0020a702e3b8d7c1d1dea11d
Rus <rus.sharafiev@gmail.com>
2023-06-25 20:56:27 +0300

    Add absolute public dir

--- web/web.go
+++ web/web.go
@@ -14,16 +14,22 @@ func Server(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	path = filepath.Join("build", path)
+	public, err := filepath.Abs("public")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join(public, path)
 
 	_, err = os.Stat(path)
 	if os.IsNotExist(err) {
-		http.ServeFile(w, r, filepath.Join("build", "index.html"))
+		http.ServeFile(w, r, filepath.Join(public, "index.html"))
 		return
 	} else if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	http.FileServer(http.Dir("build")).ServeHTTP(w, r)
+	http.FileServer(http.Dir(public)).ServeHTTP(w, r)
 }
